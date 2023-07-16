ffd733b405db2a94c3bb35c6285f3b90033ef617
Quyen Ly <vanquyen0508@gmail.com>
2023-07-16 19:22:35 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import classes from './main.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
+import classes from './main.module.css';
 document.querySelector('#app').innerHTML = `
 <div class="${classes.red}">
     hello world
